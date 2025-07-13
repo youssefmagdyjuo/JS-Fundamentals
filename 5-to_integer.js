978faf2dcd9bbf8cb73fb051e13ef0fedf260007
youssef <youssef.magdy.office@gmail.com>
2025-07-13 15:19:17 +0200

    l

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,9 +1,8 @@
-const arg = process.argv[2]; 
-const num = parseInt(arg); // تحويل القيمة إلى عدد صحيح
+const arg = process.argv[2];
+const num = parseInt(arg);
 
 if (!isNaN(num)) {
-console.log("My number:" + num);
+  console.log(`My number: ${num}`);
 } else {
-console.log("Not a number");
+  console.log("Not a number");
 }
-// 5-to_integer.js
